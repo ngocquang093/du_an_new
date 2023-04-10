2118e40e3a7296063f522bb5b9079583dfbccc11
kimtheanh <kimtheanh1208@gmail.com>
2023-04-10 08:45:15 +0700

    test

--- admin/index.php
+++ admin/index.php
@@ -9,16 +9,16 @@ require "../model/bill.php";
 require "../global.php";
 require "../model/comment.php";
 ob_start();
-if(!isset($_SESSION['user_admin'])){
+if (!isset($_SESSION['user_admin'])) {
     include "login/login.php";
 } else {
-    
+
     include "view/header.php";
-    
+
     if (isset($_GET['act'])) {
-    
+
         $act = $_GET['act'];
-    
+
         switch ($act) {
                 //            Use----------------------------------     //
             case "list_user":
@@ -43,11 +43,11 @@ if(!isset($_SESSION['user_admin'])){
                     $chuc_nang = $_POST['chuc_nang'];
                     $mat_khau = $_POST['mat_Khau'];
                     $dia_chi = $_POST['dia_chi'];
-    
+
                     insert_user($ten_khach_hang, $so_dt, $email, $chuc_nang, $mat_khau, $dia_chi);
                     $thongbao = "Thêm thành công";
                 }
-    
+
                 $list_user = user_all("", 0);
                 include 'user/add.php';
                 break;
@@ -66,14 +66,14 @@ if(!isset($_SESSION['user_admin'])){
                     $chuc_nang = $_POST['chuc_nang'];
                     $mat_khau = $_POST['mat_Khau'];
                     $dia_chi = $_POST['dia_chi'];
-    
+
                     $update = update_user($id, $ten_khach_hang, $so_dt, $email, $chuc_nang, $mat_khau, $dia_chi);
                     $thongbao = "Update thành công";
                     $list_user = user_all("", 0);
                 }
                 include 'user/list.php';
                 break;
-    
+
             case 'delete_user':
                 if (isset($_GET['id']) && ($_GET['id'] > 0)) {
                     $user = loadone_user($_GET['id']);
@@ -91,7 +91,7 @@ if(!isset($_SESSION['user_admin'])){
             case 'bill':
                 include 'view/hoadon.php';
                 break;
-    
+
             case 'bill_detail':
                 $ma_don_hang = $_GET['id'];
                 $bill = get_bill_admin($ma_don_hang);
@@ -102,34 +102,34 @@ if(!isset($_SESSION['user_admin'])){
                 // dd($bill);
                 extract($bill);
                 $totalAll = $don_gia;
-    
+
                 include 'view/bill_detail.php';
                 break;
-    
+
             case 'test':
                 include 'view/products.php';
                 break;
-    
-    
-    //            Comment ///////////////////////////
+
+
+                //            Comment ///////////////////////////
             case 'list_com':
-                if(isset($_POST['kyw'])&&($_POST['kyw']!="")){
+                if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
                     $kyw = $_POST['kyw'];
-                }else{
+                } else {
                     $kyw = "";
                 }
                 $list_cmt = cmt_all($kyw);
                 include 'comment/list.php';
                 break;
             case 'delete_cmt':
-                if(isset($_GET['id'])&&($_GET['id']>0)){
+                if (isset($_GET['id']) && ($_GET['id'] > 0)) {
                     $cmt = cmt_one($_GET['id']);
                 }
                 include 'comment/delete.php';
                 break;
 
             case 'deletecmt':
-                if(isset($_POST['delete']) && ($_POST['delete'])){
+                if (isset($_POST['delete']) && ($_POST['delete'])) {
                     $id = $_POST['id'];
                     cmt_delete($id);
                 }
@@ -147,6 +147,129 @@ if(!isset($_SESSION['user_admin'])){
                 ob_end_flush();
 
                 break;
+
+            case 'btn-add':
+                if (isset($_POST['btn-add']) && ($_POST['btn-add'])) {
+                    $ten_loai = $_POST['ten_loai'];
+                    add_loai_sp($ten_loai);
+                }
+                include 'category/add.php';
+                break;
+
+            case 'btn-edit':
+                if (isset($_GET['editId'])) {
+                    $id = $_GET['editId'];
+                    $row = get_one_loai_sp($id);
+                }
+                $categories = get_all_loai_sp();
+                include "category/edit.php";
+                break;
+            case 'list_category':
+                if (isset($_GET['deleteId'])) {
+                    $id = $_GET['deleteId'];
+                    delete_loai_sp($id);
+                }
+                $results = get_all_loai_sp();
+                include 'category/list.php';
+                break;
+                
+            case 'product-add':
+                $loai_sp = get_all_loai_sp();
+                if (isset($_POST["them_product"])) {
+                    $ten_san_pham = $_POST["ten_san_pham"];
+                    $don_gia = $_POST["don_gia"];
+                    $ma_loai = $_POST["ma_loai"];
+                    $anh_san_pham = $_FILES['anh_san_pham']['name'];
+                    $mo_ta_tom_tat = $_POST["mo_ta_tom_tat"];
+                    $ngay_tao = $_POST["ngay_tao"];
+                    $gia_khuyen_mai = $_POST["gia_khuyen_mai"];
+                    $so_luong = $_POST["so_luong"];
+
+                    if (isset($_FILES["anh_san_pham"])) {
+                        $target_dir = "../media/product/";
+                        $nameImg = $_FILES["anh_san_pham"]["name"];
+                        $target_file = $target_dir . $nameImg;
+                        $maxFileSize = 800000;
+                        $allowUpload = true;
+                        $allowTypes = ["jpg", "png", "jpeg", "gif"];
+                        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+                        if ($_FILES["anh_san_pham"]["size"] > $maxFileSize) {
+                            $error["img_size"] = "Khong duoc upload anh > " . $maxFileSize . "(Byte)";
+                            $allowUpload = false;
+                        }
+
+                        if (!in_array($imageFileType, $allowTypes)) {
+                            $error["img_type"] = "Chi duoc upload cac dinh dang jpg,png,jbeg,gif";
+                            $allowUpload = false;
+                        }
+
+                        if ($allowUpload == true) {
+                            move_uploaded_file($_FILES["anh_san_pham"]["tmp_name"], $target_file);
+                        }
+                        add_san_pham($ten_san_pham, $don_gia, $ma_loai, $anh_san_pham, $mo_ta_tom_tat, $ngay_tao, $gia_khuyen_mai, $so_luong);
+                        $message = "Thêm thành công";
+                    }
+                }
+                include 'product/add.php';
+                break;
+
+            case "update_product":
+                $loai_sp = get_all_loai_sp();
+                // $id = $_GET['editId'];
+                // $product = get_one_san_pham($id);
+
+                $error = [];
+                if (isset($_POST["edit_san_pham"])) {
+                    $ma_sp = $_POST["ma_sp"];
+                    $ten_san_pham = $_POST["ten_san_pham"];
+                    $don_gia = $_POST["don_gia"];
+                    $ma_loai = $_POST['loai_sp'];
+                    $anh_san_pham = $_FILES['anh_san_phama']['name'];
+                    $mo_ta_tom_tat = $_POST["mo_ta_tom_tat"];
+                    $ngay_tao = $_POST["ngay_tao"];
+                    $gia_khuyen_mai = $_POST["gia_khuyen_mai"];
+                    $so_luong = $_POST["so_luong"];
+
+                    if (isset($_FILES["anh_san_phama"])) {
+                        $target_dir = "../media/product/";
+                        $nameImg = $_FILES["anh_san_phama"]["name"];
+                        $target_file = $target_dir . $nameImg;
+                        $maxFileSize = 800000;
+                        $allowUpload = true;
+                        $allowTypes = ["jpg", "png", "jpeg", "gif"];
+                        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+
+                        if ($_FILES["anh_san_phama"]["size"] > $maxFileSize) {
+                            $error["img_size"] = "Khong duoc upload anh > " . $maxFileSize . "(Byte)";
+                            $allowUpload = false;
+                        }
+
+                        if (!in_array($imageFileType, $allowTypes)) {
+                            $error["img_type"] = "Chi duoc upload cac dinh dang jpg,png,jbeg,gif";
+                            $allowUpload = false;
+                        }
+
+                        if ($allowUpload == true) {
+                            move_uploaded_file($_FILES["anh_san_phama"]["tmp_name"], $target_file);
+                        }
+                        edit_san_pham($ma_san_pham, $ten_san_pham, $don_gia, $anh_san_pham, $mo_ta_tom_tat, $ngay_tao, $gia_khuyen_mai, $so_luong);
+                        $message = "update thành công";
+                    }
+                }
+
+                include 'product/edit.php';
+                break;
+            case 'list_products':
+                if (isset($_GET['deleteId'])) {
+                    $id = $_GET['deleteId'];
+                    delete_san_pham($id);
+                }
+                $results = get_all_san_pham();
+                include 'product/list.php';
+                break;
+
             default:
                 include "view/home.php";
         }
@@ -156,4 +279,3 @@ if(!isset($_SESSION['user_admin'])){
         include "view/footer.php";
     }
 }
-
